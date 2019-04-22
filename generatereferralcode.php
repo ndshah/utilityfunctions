f611cbc470391618335dce6616feffaf8fd63120
Nirav Shah <nd.07it042@gmail.com>
2019-04-22 14:14:16 +0530

    Create generatereferralcode.php
    Generate referral code while user signup, or use same logic for another purpose

---
+++ generatereferralcode.php
@@ -0,0 +1,42 @@
+<?php
+// combine string with random str and check uniqueness , if not unique, recurse
+public function generateReferralCode($str = '', $table = 'user', $field = 'referral_code')
+{
+    if($str!=''){
+        $name = getInitialChars($str);
+        $code = $name . generateRandomString(5);
+        $code = strtoupper($code);
+        $result = mysql_query('select count(*) from '.$table.' where `'.$field.'` = "'.$code.'" AND `'.$field.'` IS NOT NULL');
+        $rowcount = mysql_num_rows($result);
+        if (!empty($arr)) {
+            return generateReferralCode($name);
+        } else {
+            return $code;
+        }
+    }
+}
+
+// get initial chars from user's name, 3 chars and if not, append 0s
+public function getInitialChars($name)
+{
+    $name = str_replace(' ', '', $name);
+    $len = strlen($name);
+    if ($len < 3) {
+        $rem = 3 - $len;
+        $str = $name . str_repeat('0', $rem);
+    } else {
+        $str = substr($name, 0, 3);
+    }
+    return $str;
+}
+
+public function generateRandomString($length = 10)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
